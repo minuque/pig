3fa19e4b2501d35189e5b199dec4856acf51bf98
HappyXqw <1053796416@qq.com>
2026-08-21 11:53:40 +0800

    fix(web): wrap Tooltip with TooltipProvider

--- apps/web/src/components/ui/tooltip/Tooltip.vue
+++ apps/web/src/components/ui/tooltip/Tooltip.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { TooltipRootEmits, TooltipRootProps } from "reka-ui";
 import type { ComputedRef } from "vue";
-import { TooltipRoot, useForwardPropsEmits } from "reka-ui";
+import { TooltipProvider, TooltipRoot, useForwardPropsEmits } from "reka-ui";
 
 const props = defineProps<TooltipRootProps>();
 const emits = defineEmits<TooltipRootEmits>();
@@ -10,7 +10,9 @@ const forwarded = useForwardPropsEmits(props, emits) as ComputedRef<TooltipRootP
 </script>
 
 <template>
-  <TooltipRoot v-slot="slotProps" data-slot="tooltip" v-bind="forwarded">
-    <slot v-bind="slotProps" />
-  </TooltipRoot>
+  <TooltipProvider>
+    <TooltipRoot v-slot="slotProps" data-slot="tooltip" v-bind="forwarded">
+      <slot v-bind="slotProps" />
+    </TooltipRoot>
+  </TooltipProvider>
 </template>
